67dc59f0cbc3c49669ef1b0b4a28579f8c662e3b
hanajjang25 <93505567+hanajjang25@users.noreply.github.com>
2024-03-21 16:48:02 +0900

    Update login.dart

--- login.dart
+++ login.dart
@@ -7,8 +7,41 @@ class login extends StatelessWidget {
       appBar: AppBar(
         title: Text('로그인'),
       ),
-      body: Center(
-        child: Text('로그인', style: TextStyle(fontSize: 24.0)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 150),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Card(
+                elevation: 3,
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'ID',
+                          hintText: 'Enter your ID',
+                        ),
+                      ),
+                      SizedBox(height: 15),
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Password',
+                          hintText: 'Enter secure password',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
