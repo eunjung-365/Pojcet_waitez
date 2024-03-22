e0790c570331f0ff5b0dce5f435902d2af8f22b9
hanajjang25 <93505567+hanajjang25@users.noreply.github.com>
2024-03-22 15:53:43 +0900

    Update login.dart

--- login.dart
+++ login.dart
@@ -10,15 +10,24 @@ class login extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            SizedBox(height: 150),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
+              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 70),
               child: Card(
                 elevation: 3,
+                shadowColor: Colors.black,
                 child: Padding(
                   padding: EdgeInsets.all(20),
                   child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
+                      Text(
+                        '로그인',
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: 2.0),
+                      ),
+                      SizedBox(height: 50),
                       TextField(
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
@@ -34,6 +43,23 @@ class login extends StatelessWidget {
                           labelText: 'Password',
                           hintText: 'Enter secure password',
                         ),
+                      ),
+                      SizedBox(height: 50),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment
+                            .center, // 가운데 정렬을 위한 MainAxisAlignment 설정
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5),
+                              ),
+                            ),
+                            child: Text('로그인'),
+                          ),
+                        ],
                       ),
                     ],
                   ),
