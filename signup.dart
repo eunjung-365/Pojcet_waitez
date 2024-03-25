47bc536079a28cfe8ce441f3360320385b11a78b
hanajjang25 <93505567+hanajjang25@users.noreply.github.com>
2024-03-25 18:51:00 +0900

    Update signup.dart

--- signup.dart
+++ signup.dart
@@ -20,6 +20,14 @@ class signup extends StatelessWidget {
                 ),
               ),
             ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20), // 양쪽에 50의 여백 추가
+              child: Container(
+                height: 1,
+                width: double.maxFinite,
+                color: Colors.black,
+              ),
+            ),
             SizedBox(height: 20),
             Padding(
               padding: EdgeInsets.only(left: 20.0, right: 70.0),
@@ -30,6 +38,7 @@ class signup extends StatelessWidget {
                     borderSide: BorderSide(color: Colors.black),
                   ),
                   labelText: '이름',
+                  hintText: '※영어, 한글만 입력 가능합니다.',
                 ),
               ),
             ),
@@ -46,21 +55,39 @@ class signup extends StatelessWidget {
                           borderSide: BorderSide(color: Colors.black),
                         ),
                         labelText: '아이디',
+                        hintText: '※아이디는 한번 설정 후 변경이 불가능합니다.',
                       ),
                     ),
                   ),
                   SizedBox(width: 10),
-                  Container(
+                  SizedBox(
                     width: 40,
                     height: 60,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    child: Center(
-                      child: Text(
-                        '중복확인',
-                        style: TextStyle(
-                          fontSize: 15,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>((states) {
+                          // 버튼의 배경색 지정
+                          if (states.contains(MaterialState.disabled)) {
+                            return Colors.grey; // 비활성 상태일 때 배경색
+                          }
+                          return Colors.transparent; // 활성 상태일 때 배경색
+                        }),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.black), // 테두리 설정
+                            borderRadius: BorderRadius.circular(0), // 모서리 설정
+                          ),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '중복확인',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.black, // 텍스트 색상 설정
+                          ),
                         ),
                       ),
                     ),
@@ -78,6 +105,7 @@ class signup extends StatelessWidget {
                     borderSide: BorderSide(color: Colors.black),
                   ),
                   labelText: '비밀번호',
+                  hintText: '※영어, 숫자, 특수문자 중 3종류 이상, 8자 이상',
                 ),
               ),
             ),
@@ -98,17 +126,34 @@ class signup extends StatelessWidget {
                     ),
                   ),
                   SizedBox(width: 10),
-                  Container(
+                  SizedBox(
                     width: 40,
                     height: 60,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    child: Center(
-                      child: Text(
-                        '확인',
-                        style: TextStyle(
-                          fontSize: 15,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>((states) {
+                          // 버튼의 배경색 지정
+                          if (states.contains(MaterialState.disabled)) {
+                            return Colors.grey; // 비활성 상태일 때 배경색
+                          }
+                          return Colors.transparent; // 활성 상태일 때 배경색
+                        }),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.black), // 테두리 설정
+                            borderRadius: BorderRadius.circular(0), // 모서리 설정
+                          ),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '확인',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.black, // 텍스트 색상 설정
+                          ),
                         ),
                       ),
                     ),
@@ -129,21 +174,39 @@ class signup extends StatelessWidget {
                           borderSide: BorderSide(color: Colors.black),
                         ),
                         labelText: '전화번호',
+                        hintText: '예) 010-1234-5678',
                       ),
                     ),
                   ),
                   SizedBox(width: 10),
-                  Container(
+                  SizedBox(
                     width: 40,
                     height: 60,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    child: Center(
-                      child: Text(
-                        '전화번호 요청',
-                        style: TextStyle(
-                          fontSize: 12,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>((states) {
+                          // 버튼의 배경색 지정
+                          if (states.contains(MaterialState.disabled)) {
+                            return Colors.grey; // 비활성 상태일 때 배경색
+                          }
+                          return Colors.transparent; // 활성 상태일 때 배경색
+                        }),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.black), // 테두리 설정
+                            borderRadius: BorderRadius.circular(0), // 모서리 설정
+                          ),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '인증번호 요청',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.black, // 텍스트 색상 설정
+                          ),
                         ),
                       ),
                     ),
@@ -168,17 +231,34 @@ class signup extends StatelessWidget {
                     ),
                   ),
                   SizedBox(width: 10),
-                  Container(
+                  SizedBox(
                     width: 40,
                     height: 60,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    child: Center(
-                      child: Text(
-                        '확인',
-                        style: TextStyle(
-                          fontSize: 15,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.resolveWith<Color>((states) {
+                          // 버튼의 배경색 지정
+                          if (states.contains(MaterialState.disabled)) {
+                            return Colors.grey; // 비활성 상태일 때 배경색
+                          }
+                          return Colors.transparent; // 활성 상태일 때 배경색
+                        }),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            side: BorderSide(color: Colors.black), // 테두리 설정
+                            borderRadius: BorderRadius.circular(0), // 모서리 설정
+                          ),
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          '확인',
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.black, // 텍스트 색상 설정
+                          ),
                         ),
                       ),
                     ),
